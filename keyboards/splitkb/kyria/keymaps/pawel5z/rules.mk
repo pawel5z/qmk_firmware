804ff41a45d9920361239994dc5d1d21328ba591
Paweł Szmergała <pawelszme@gmail.com>
2023-12-09 21:40:44 +0100

    Enable encoder mapping

--- keyboards/splitkb/kyria/keymaps/pawel5z/rules.mk
+++ keyboards/splitkb/kyria/keymaps/pawel5z/rules.mk
@@ -3,3 +3,6 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 BOOTLOADER = caterina      # Because of Pro Micro MCU used
+# Encoders documentation
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_encoders.md
+ENCODER_MAP_ENABLE = yes   # Enable layers for encoders
