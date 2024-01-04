8e48c74cb16a94146ad8987858abeb81d6bfaa77
Paweł Szmergała <pawelszme@gmail.com>
2024-01-04 18:22:02 +0100

    Remove num lock status display

--- keyboards/splitkb/kyria/keymaps/pawel5z/keymap.c
+++ keyboards/splitkb/kyria/keymaps/pawel5z/keymap.c
@@ -277,7 +277,6 @@ bool oled_task_user(void) {
 
         // Write host Keyboard LED Status to OLEDs
         led_t led_usb_state = host_keyboard_led_state();
-        oled_write_P(led_usb_state.num_lock    ? PSTR("NUMLCK ") : PSTR("       "), false);
         oled_write_P(led_usb_state.caps_lock   ? PSTR("CAPLCK ") : PSTR("       "), false);
         oled_write_P(led_usb_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
     } else {
